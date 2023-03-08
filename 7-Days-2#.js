c5d53cecbf5c9143f08ec09a0708f3ddf8a33398
Bernardo <beprieto47@gmail.com>
2023-03-08 15:38:56 -0300

    atualizando projetos

--- 7-Days-2#.js
+++ 7-Days-2#.js
@@ -4,10 +4,10 @@ var nome = prompt ("Olá novo usuário! Qual é o seu nome?");
 var idade = prompt ("Quantos anos você tem?");
 var linguagem = prompt ("Qual linguagem de programação você está estudando?");
 console.log("Olá " + nome + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!");
-var resposta = prompt ("você está gostando de estudar " + linguagem + "responda com o número 1 para sim ou 2 para não");
-if (resposta == 1){
+var resposta = prompt ("você está gostando de estudar " + linguagem);
+if (resposta == "sim"){
    console.log("Muito bom! Continue estudando e você terá muito sucesso.")
 }
-if (resposta == 2){
+if (resposta == "nao"){
     console.log("Ahh que pena... Já tentou aprender outras linguagens?")
 }
---
+++ calculador.js
@@ -0,0 +1,4 @@
+var primeiro = parseInt(prompt ("Digite o primeiro número: "))
+var segundo = parseInt(prompt ("Digite o segundo número: "))
+var resultado = primeiro + segundo
+console.log (primeiro + "+" + segundo + " = " + resultado)
