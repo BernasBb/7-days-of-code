74a022c5ca1021f5db788fd5bb14e230be86940c
BernasBb <123491333+BernasBb@users.noreply.github.com>
2023-03-08 19:24:29 -0300

    Delete calculadora.js

--- calculadora.js
+++
@@ -1,4 +0,0 @@
-var primeiro = parseInt(prompt ("Digite o primeiro número: "))
-var segundo = parseInt(prompt ("Digite o segundo número: "))
-var resultado = primeiro + segundo
-console.log (primeiro + "+" + segundo + " = " + resultado)
